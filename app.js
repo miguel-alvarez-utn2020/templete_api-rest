5296f6c3a374df6264890bb0337af80964345af7
miguel <miguel.Alvarez@syloper.com>
2022-07-11 22:52:31 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,15 @@
+require ("dotenv").config()
+
+const express = require("express");
+
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+const port = process.env.PORT || 3000;
+
+app.listen( port, () => {
+    console.log(`Tu app esta por http://localhost: ${port}`)
+})
